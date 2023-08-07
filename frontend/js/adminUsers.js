77be8f78feaee9d9ac78abfa4c7df7341dc1346e
sammykinuthia <kinuthiasamuel05@gmail.com>
2023-08-07 17:10:53 +0300

    users filter functionality

--- frontend/js/adminUsers.js
+++ frontend/js/adminUsers.js
@@ -1,32 +1,47 @@
 import { useFetchGet, useFetchPost } from "./auth/utilities.js"
 
 
-const users = document.getElementById("users")
-let usersList = ""
-useFetchGet("http://localhost:3000/users").then(r=>{
-    console.log(r.users)
-    for(let user of r.users){
-        console.log(user);
-        usersList += 
-    `<div class="user-card" id=${user.id}>
-        <div class="user-select">
-            <input type="checkbox" >
-        </div>
-        <div class="user-name-section">
-            <h4 class="user-name">${user.full_name}</h4>
-        </div>
-        <div class="user-email-section">
-            <p class="user-email">${user.email}</p>
-        </div>
-        <div class="user-project-section">
-            <p class="user-project">${user.project}</p>
-        </div>
-        <div class="user-verified">
-            <p class="user-project">${user.is_verified ? '<iconify-icon class="icon-correct" icon="icon-park-twotone:correct"></iconify-icon>': '<iconify-icon class="icon-waiting" icon="streamline:interface-page-controller-loading-half-progress-loading-load-half-wait-waiting"></iconify-icon>'}</p>
-        </div>
-    </div>`
-    }
-    users.innerHTML = usersList
+const usersSection = document.getElementById("users")
+const usersFielter = document.getElementById("users-list-sort")
+renderUsers()
+usersFielter.addEventListener("change", e => {
+    renderUsers(usersFielter.value)
 })
+function renderUsers(fielter = 'all') {
+    useFetchGet("http://localhost:3000/users").then(r => {
+        let users = r.users
+        if (fielter == "unverified") {
+            users = users.filter(user => !user.is_verified)
+        }
+        else if (fielter == "verified") {
+            users = users.filter(user => user.is_verified )
+        }
+    
+        console.log(users);
+        let usersList = ""
+        for (let user of users) {
+            // console.log(user);
+            usersList +=
+                `<div class="user-card" id=${user.id}>
+                    <div class="user-select">
+                        <input type="checkbox" >
+                    </div>
+                    <div class="user-name-section">
+                        <h4 class="user-name">${user.full_name}</h4>
+                    </div>
+                    <div class="user-email-section">
+                        <p class="user-email">${user.email}</p>
+                    </div>
+                    <div class="user-project-section">
+                        <p class="user-project">${user.project}</p>
+                    </div>
+                    <div class="user-verified">
+                        <p class="user-project">${user.is_verified ? '<iconify-icon class="icon-correct" icon="icon-park-twotone:correct"></iconify-icon>' : '<iconify-icon class="icon-waiting" icon="streamline:interface-page-controller-loading-half-progress-loading-load-half-wait-waiting"></iconify-icon>'}</p>
+                    </div>
+                </div>`
+        }
+        usersSection.innerHTML = usersList
+    })
 
+}
 
