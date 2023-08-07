6ae08c8a54eee19eeec03a041dba34d02e72ec90
sammykinuthia <kinuthiasamuel05@gmail.com>
2023-08-07 14:30:03 +0300

    get users from db

--- frontend/js/adminUsers.js
+++ frontend/js/adminUsers.js
@@ -4,27 +4,29 @@ import { useFetchGet, useFetchPost } from "./auth/utilities.js"
 const users = document.getElementById("users")
 let usersList = ""
 useFetchGet("http://localhost:3000/users").then(r=>{
-    // console.log(r.users)
+    console.log(r.users)
     for(let user of r.users){
-        
-        usersList+= `<div class="user-card">
+        console.log(user);
+        usersList += 
+    `<div class="user-card" id=${user.id}>
         <div class="user-select">
-            <input type="checkbox" id="">
+            <input type="checkbox" >
         </div>
         <div class="user-name-section">
-            <h4 class="user-name">Sammy kigo</h4>
+            <h4 class="user-name">${user.full_name}</h4>
         </div>
         <div class="user-email-section">
-            <p class="user-email">Sammykigo@gmail.com</p>
+            <p class="user-email">${user.email}</p>
         </div>
         <div class="user-project-section">
-            <p class="user-project">Dam construction</p>
+            <p class="user-project">${user.project}</p>
+        </div>
+        <div class="user-verified">
+            <p class="user-project">${user.is_verified ? '<iconify-icon class="icon-correct" icon="icon-park-twotone:correct"></iconify-icon>': '<iconify-icon class="icon-waiting" icon="streamline:interface-page-controller-loading-half-progress-loading-load-half-wait-waiting"></iconify-icon>'}</p>
         </div>
     </div>`
     }
-    
+    users.innerHTML = usersList
 })
 
-
-users.innerHTML = usersList
 
