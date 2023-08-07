c3994ccc03e1fe7d1cbf70b68e89a3ffc652dee5
Eva-Wamuyu <rutheve.eva@gmail.com>
2023-08-07 17:48:14 +0300

    new file:   DatabaseHelpers/index.js 	deleted:    ../services/DatabaseHelpers/index.js

---
+++ server/DatabaseHelpers/index.js
@@ -0,0 +1,32 @@
+import { sqlConfig } from "../Config/config.js";
+
+import mssql from 'mssql'
+
+
+
+export class DB {
+
+    static async exec(storedProcedure, data = {}) {
+        try {
+            let request = await (await mssql.connect(sqlConfig)).request();
+            request = this.addData(request, data);
+            console.log("called");
+            return request.execute(storedProcedure);
+        } catch (error) {
+            console.log(error);
+            return error
+        }
+    }
+
+    static addData(req, data = {}) {
+        const keys = Object.keys(data);
+
+        keys.forEach((keyName) => {
+            req.input(keyName, data[keyName]);
+        });
+
+        return req;
+
+    }
+
+}
