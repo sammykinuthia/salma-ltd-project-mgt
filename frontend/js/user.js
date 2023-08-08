761c614e232982a25f113bc876d809301f78ab49
Eva-Wamuyu <rutheve.eva@gmail.com>
2023-08-08 13:38:29 +0300

    modified:   frontend/js/user.js 	modified:   frontend/user/index.html 	modified:   services/EmailService/newUser.js 	modified:   services/index.js

--- frontend/js/user.js
+++ frontend/js/user.js
@@ -10,7 +10,15 @@ async function renderProject(){
         const user = getCurrentUser()
         console.log(user);
         const [res, code] = await useFetchGet(`http://localhost:3000/projects/${user.id}`)
+        if(code == 200){
+
+       
         console.log(res);
+        document.querySelector(".project-title").innerHTML = res.data.name
+        }
+        else if(code == 404){
+            document.querySelector(".project-title").innerHTML = "No Assigned Project At the Moment";
+        }
 
     } catch (error) {
         console.log(error);
