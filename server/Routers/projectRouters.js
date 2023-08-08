27c877921ee0a922c4f49ce70fca51edf7a8cfbe
sammykinuthia <kinuthiasamuel05@gmail.com>
2023-08-08 14:30:29 +0300

    bugs ...

--- server/Routers/projectRouters.js
+++ server/Routers/projectRouters.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getProjects, createProject,getProject,getUserProject,getUserProjectsHistory, deleteProject, assignProject, getAssignedProject,getUsersForAproject, getProjectByUserId} from "../Controllers/projectControllers.js";
+import { getProjects, createProject,getProject,getUserProject,getUserProjectsHistory,getUsersForProject, deleteProject, assignProject, getAssignedProject,getUsersForAproject, getProjectByUserId} from "../Controllers/projectControllers.js";
 import { validateUser } from "../Middleware/userValidation.js";
 
 
@@ -9,6 +9,7 @@ projectRouter.get('/',validateUser,getProjects)
 projectRouter.post('/',validateUser,createProject)
 projectRouter.get('/:id',validateUser,getProject)
 // projectRouter.get('/:id',validateUser,getProjectByUserId)
+projectRouter.post('/users',validateUser,getUsersForProject)
 projectRouter.get('/user/',validateUser, getUserProject)
 projectRouter.get('/user/hist/',validateUser, getUserProjectsHistory)
 projectRouter.post('/assign', validateUser, assignProject)
