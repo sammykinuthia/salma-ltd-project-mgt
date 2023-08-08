a7406108623eec86a03218d6dc3c0f9772586a4d
sammykinuthia <kinuthiasamuel05@gmail.com>
2023-08-08 08:03:56 +0300

    get users fix

--- frontend/js/global.js
+++ frontend/js/global.js
@@ -1,22 +1,27 @@
 const logout = document.getElementById("logout")
+
 logout.addEventListener("click",()=>{
-    sessionStorage.removeItem("currentUser")
-    localStorage.removeItem("token")
+    try {
+        sessionStorage.removeItem("currentUser")
+        localStorage.removeItem("token")
+    } catch (error) {
+        console.log(error);
+    }
+
     setInterval(window.location.replace("/auth/login.html"),4000)
     
 })
 
 
-function formatDate(dateString) {
+export function formatDate(dateString) {
     const months = ["Jan","Feb","March","April","May",
                      "June","July","Aug","Sep","Oct","Nov","Dec"   ]
     const date = new Date(dateString);
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
-   
     return `${day}-${months[month-1]}-${year}`;
   }
   
   
-  export {formatDate}
+ 
