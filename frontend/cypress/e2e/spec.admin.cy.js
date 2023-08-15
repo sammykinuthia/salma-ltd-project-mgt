89a97d96df13d6f72f942be4a22136bd8d3a518f
Eva-Wamuyu <rutheve.eva@gmail.com>
2023-08-15 15:05:29 +0300

    Changes to be committed: 	modified:   frontend/cypress/e2e/spec.admin.cy.js 	modified:   frontend/cypress/e2e/spec.login.user.cy.js 	modified:   frontend/cypress/support/commands.js

--- frontend/cypress/e2e/spec.admin.cy.js
+++ frontend/cypress/e2e/spec.admin.cy.js
@@ -1,22 +1,37 @@
 /// <reference types='Cypress'/>
 
-import { url } from "./spec.login.user.cy";
+//import { url } from "./spec.login.user.cy";
 
 const project_url = '/admin/project.html';
-describe("Tests for admin page",()=>{
-  it('has projects divs contained in it once there are projects',()=>{
-    cy.login('joystone','password');
+
+describe("Tests for admin page", () => {
+
+  it('has projects divs contained in it once there are projects', () => {
+    cy.login('joystone', 'password');
     cy.get('.main-body').find('.project-card').as('projectCard')
   })
 
-  it('The first project div is clickable',()=>{
-    cy.login('joystone','password');
+  it('The first project div is clickable', () => {
+    cy.login('joystone', 'password');
     cy.get('.project-card').first().click();
-    cy.location('pathname').should('equal',project_url)
+    cy.location('pathname').should('equal', project_url)
 
   })
 
-  
+  it('The project divs are clickable', () => {
+    cy.login('joystone', 'password');
+    cy.get('.project-card').as('cards')
+    cy.get('@cards').then((el)=>{
+      el.click();
+      cy.location('pathname').should('include', 'project')
+      cy.go('back')
+    });
+
+      
+
+
+    });
+    //cy.visit(`${url}admin`
 })
 
 
