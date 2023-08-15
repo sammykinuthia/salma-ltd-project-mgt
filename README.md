3285ce5236c84c4be90f5c1d69e1c8955760ea48
Eva-Wamuyu <rutheve.eva@gmail.com>
2023-08-15 14:12:54 +0300

    Changes to be committed: 	modified:   README.md 	modified:   frontend/cypress/e2e/spec.admin.cy.js 	modified:   frontend/package-lock.json

--- README.md
+++ README.md
@@ -4,27 +4,35 @@
 Prerequisites
 
 Have http-server installed
-```
+```bash
 cd frontend
 http-server
 ```
 
 ##### Backend/Server
-```
+```bash
 cd server
 npm install
 npm run dev
 ```
 
 ##### Services
-```
+```bash
 cd services
 npm install
-npm run dev
+npm start
 ```
 
 ### Runnning Tests
 ##### Unit Tests for Backend
-
+```bash
+cd server
+npm test
+```
 ##### Unit Tests for Frontend
+
+```bash
+cd frontend
+
+```
 ##### e2e tests
--- frontend/cypress/e2e/spec.admin.cy.js
+++ frontend/cypress/e2e/spec.admin.cy.js
@@ -1,11 +1,22 @@
 /// <reference types='Cypress'/>
 
+import { url } from "./spec.login.user.cy";
 
+const project_url = '/admin/project.html';
 describe("Tests for admin page",()=>{
   it('has projects divs contained in it once there are projects',()=>{
     cy.login('joystone','password');
-    cy.get('.main-body').find('.project-card')
+    cy.get('.main-body').find('.project-card').as('projectCard')
   })
+
+  it('The first project div is clickable',()=>{
+    cy.login('joystone','password');
+    cy.get('.project-card').first().click();
+    cy.location('pathname').should('equal',project_url)
+
+  })
+
+  
 })
 
 
